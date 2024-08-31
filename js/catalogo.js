d6c2eecee5975ba80a6ad681b4acc7819076c44e
FernandoRC789 <nickfernando789@gmail.com>
2024-08-31 16:20:35 -0500

    correccion de comentarios

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,25 +1,6 @@
-// Espera a que el DOM esté completamente cargado
-/*document.addEventListener('DOMContentLoaded', function() {
-    // Obtén el elemento select
-    var selectElement = document.getElementById('colorAuto');
-    
-    // Añade un event listener para el cambio en el select
-    selectElement.addEventListener('change', function() {
-        // Obtén la opción seleccionada
-        var selectedOption = this.options[this.selectedIndex];
-        
-        // Obtén el valor del atributo value (la ruta de la imagen)
-        var imageUrl = selectedOption.value;
-        
-        // Cambia la fuente de la imagen
-        var imageElement = document.getElementById('imgtoyota');
-        imageElement.src = imageUrl;
-    });
-});*/
 
-//
 document.addEventListener('DOMContentLoaded', function() {
-    // Función para manejar el cambio de imagen según el color seleccionado
+    // Función para cmabiar la imagen segun el color seleccionado 
     function setupColorChange(selectorId, imgId) {
         const colorSelect = document.getElementById(selectorId);
         const carImage = document.getElementById(imgId);
@@ -29,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Configurar el cambio de imagen para cada producto
+    // se llama la funcion para configurar el camboi
     setupColorChange('toyota-color', 'toyota-img');
     setupColorChange('nissan-color', 'nissan-img');
     setupColorChange('bomber-capucha-color', 'bomber-capucha-img');
